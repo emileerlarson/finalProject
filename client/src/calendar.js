88e34ff5cdf1e7a8604432fc434e2d9fdf6cb080
JamesTown-625 <{JamesTown-625}@users.noreply.github.com>
2018-03-01 21:14:00 -0700

    Made the initial react calendar component with react-calendar npm package

--- client/src/calendar.js
+++ client/src/calendar.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
-import Calendar from 'react-calendar';
+import Calendar from 'react-calendar/dist/entry.nostyle';
 
-class App extends Component {
+class MyApp extends React.Component {
+
   state = {
     date: new Date(),
+    minDetail: 'year',
   }
 
   onChange = date => this.setState({ date })
 
   render() {
+
+    const minDetail = this.state.minDetail;
+
     return (
       <div>
-        <h1>HEllO WORLD</h1>
         <Calendar
           onChange={this.onChange}
+          minDetail={minDetail}
           value={this.state.date}
         />
       </div>
     );
   }
 }
-
-export default App;
